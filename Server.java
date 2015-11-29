8f6a127a8bd02db4ded0fba26aa5c84d0dbecfcd
Dylan Chen <hjchen2005@gmail.com>
2015-11-29 14:25:54 -0600

    Update Server.java

--- Server.java
+++ Server.java
@@ -1,4 +1,5 @@
 import java.lang.Thread;
+import java.io.*;
 
 public class Server implements Runnable{
   ServerSocket serverSocket;
@@ -12,6 +13,31 @@ public class Server implements Runnable{
   public void run(){
     try{
       serverSocket = new ServerSocket (getPortNumber());
+      InputStream in = server.getInputStream();
+        OutputStream out = server.getOutputStream();
+
+        // write a byte
+        out.write(42);
+
+        // write a newline or carriage return delimited string
+        PrintWriter pout = new PrintWriter( out, true );
+        pout.println("Hello!");
+
+        // read a byte
+        byte back = (byte)in.read();
+
+        // read a newline or carriage return delimited string
+        BufferedReader bin =
+          new BufferedReader( new InputStreamReader( in ) );
+        String response = bin.readLine();
+
+        // send a serialized Java object
+        ObjectOutputStream oout = new
+        ObjectOutputStream( out );
+        oout.writeObject( new java.util.Date() );
+        oout.flush();
+      
+        serverSocket.close();
     }
     catch(IOExceptio e){
       System.out.println(e);
