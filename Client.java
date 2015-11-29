18164f035aba050bc8a93495c2ef78b3c90c6d74
Dylan Chen <hjchen2005@gmail.com>
2015-11-29 15:02:37 -0600

    Update Client.java

--- Client.java
+++ Client.java
@@ -1,11 +1,42 @@
 import java.util.Thread;
-
+// http://chimera.labs.oreilly.com/books/1234000001805/ch13.html#learnjava3-CHP-13-SECT-1.1
 public class Client implements Runnable{
 
   @Override
   public void Run(){
     try {
         Socket sock = new Socket("127.0.0.1.", Server.getPortNumber());
+        ServerSocket listener = new ServerSocket( Server.getPortNumber() );
+
+        while ( !finished ) {
+            Socket client = listener.accept();  // wait for connection
+
+            InputStream in = client.getInputStream();
+            OutputStream out = client.getOutputStream();
+
+            // read a byte
+            byte someByte = (byte)in.read();
+
+            // read a newline or carriage-return-delimited string
+            BufferedReader bin =
+              new BufferedReader( new InputStreamReader( in ) );
+            String someString = bin.readLine();
+
+            // write a byte
+            out.write(43);
+
+            // say goodbye
+            PrintWriter pout = new PrintWriter( out, true );
+            pout.println("Goodbye!");
+      
+            // read a serialized Java object
+            ObjectInputStream oin = new ObjectInputStream( in );
+            Date date = (Date)oin.readObject();
+
+            client.close();
+        }
+
+        listener.close();
       } catch ( UnknownHostException e ) {
           System.out.println("Can't find host.");
       } catch ( IOException e ) {
