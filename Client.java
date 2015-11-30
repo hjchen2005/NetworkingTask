7e3ac9f0588181df89acb60ab435d0e50c2787e5
Dylan Chen <hjchen2005@gmail.com>
2015-11-29 19:50:04 -0500

    Update Client.java

--- Client.java
+++ Client.java
@@ -1,21 +1,61 @@
-import java.util.Thread;
-import java.net.*;
+import java.lang.Thread;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
 import java.io.*;
 
-// http://chimera.labs.oreilly.com/books/1234000001805/ch13.html#learnjava3-CHP-13-SECT-1.1
-public class Client implements Runnable{
-
-  Socket sock;
+public class Server implements Runnable{
+  ServerSocket serverSocket;
+  private final int port = 1337 ;
   
-  public Client(InetAddress addr){
-     sock = new Socket(addr, Server.getPortNumber());
+  public static int getPortNumber(){
+    return port;
   }
   
   @Override
-  public void Run(){
+  public void run(){
+    try{
+      serverSocket = new ServerSocket (getPortNumber());
+      InputStream in = server.getInputStream();
+        OutputStream out = server.getOutputStream();
+
+        // write a byte
+        out.write(42);
+
+        // write a newline or carriage return delimited string
+        PrintWriter pout = new PrintWriter( out, true );
+        pout.println("Hello!");
+
+        // read a byte
+        byte back = (byte)in.read();
+
+        // read a newline or carriage return delimited string
+        BufferedReader bin =
+          new BufferedReader( new InputStreamReader( in ) );
+        String response = bin.readLine();
+
+        // send a serialized Java object
+        ObjectOutputStream oout = new
+        ObjectOutputStream( out );
+        oout.writeObject( new java.util.Date() );
+        oout.flush();
+      
+        serverSocket.close();
+    }
+    catch(IOException e){
+      System.out.println(e);
+    } // end 
+    
+    /*
+    After a connection is established, a server application uses the same kind of Socket object for its side of the communications. 
+    However, to accept a connection from a client, it must first create a ServerSocket, bound to the correct port.
+    
+    First, our server creates a ServerSocket attached to port 1234. On some systems, there are rules about which ports an application can use. Port numbers below 1024 are usually reserved for system processes and standard, well-known services, so we pick a port number outside of this range. The ServerSocket is created only once; thereafter, we can accept as many connections as arrive.
+Next, we enter a loop, waiting for the accept() method of the ServerSocket to return an active Socket connection from a client. When a connection has been established, we perform the server side of our dialog, then close the connection and return to the top of the loop to wait for another connection. Finally, when the server application wants to stop listening for connections altogether,
+it calls the close() method of the ServerSocket.
+    */
     try {
-        //Socket sock = new Socket("127.0.0.1.", Server.getPortNumber());
-        ServerSocket listener = new ServerSocket(Server.getPortNumber() );
+        ServerSocket listener = new ServerSocket(getPortNumber());
 
         while ( !finished ) {
             Socket client = listener.accept();  // wait for connection
@@ -46,10 +86,8 @@ public class Client implements Runnable{
         }
 
         listener.close();
-      } catch ( UnknownHostException e ) {
-          System.out.println("Can't find host.");
-      } catch ( IOException e ) {
-          System.out.println("Error connecting to host.");
-      }
     }
+    catch (IOException e ) {}
+    catch (ClassNotFoundException e2){}// end catch
+  }// end run
 }
