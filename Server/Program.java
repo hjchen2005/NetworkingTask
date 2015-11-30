65c5926c91c01cd80618e4d92b5b8d9011133a73
Dylan Chen <hjchen2005@gmail.com>
2015-11-29 21:28:45 -0500

    Create Program.java

---
+++ Server/Program.java
@@ -0,0 +1,11 @@
+// Entry Point
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Server server = new Server();
+		server.run();
+	}
+
+}
