1d50c392494f2ebbdb4581767d85728c6be22fb1
Dylan Chen <hjchen2005@gmail.com>
2015-11-29 21:56:56 -0500

    Create MainClient.java

---
+++ Client/MainClient.java
@@ -0,0 +1,12 @@
+package Client;
+
+import java.io.IOException;
+
+public class MainClient {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		Client client = new Client();
+	}
+
+}
