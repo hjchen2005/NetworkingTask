ebdddee4d49cc77871adfcd4eaee290d7704d105
Dylan Chen <hjchen2005@gmail.com>
2015-12-01 14:40:55 -0500

    Update Server.java

--- Server/Server.java
+++ Server/Server.java
@@ -1,52 +1,150 @@
 package Server;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Date;
-import java.io.*;
 
-public class Server implements Runnable  {
-	//boolean finished = false;
-  ServerSocket listener;
-  private final static int port = 1337 ;
-  private boolean finished = false;
-  
-  public static int getPortNumber(){
-    return port;
-  }
-  
-  @Override
-  public void run() {
-	  try {
-		listener = new ServerSocket(getPortNumber());
-	} catch (IOException e1) {
-		// TODO Auto-generated catch block
-		e1.printStackTrace();
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+
+public class Server extends JFrame{
+	
+	public final static int portNumber = 1339;
+	private final int maxQueueLength =100;
+	private boolean finished = false;
+	
+	//private String message = "";
+	private JTextField textField;
+	private JTextArea chatWindow;
+	private ObjectInputStream input;
+	private ObjectOutputStream output;
+	private ServerSocket server;
+	private Socket connection;
+	
+	public Server(){
+		super("Server program");
+		textField = new JTextField();
+		textField.setEditable(false);
+		textField.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				sendMessage(e.getActionCommand());
+				textField.setText("");
+			}
+		});
+		add(textField,BorderLayout.NORTH);
+		chatWindow = new JTextArea();
+		add(new JScrollPane(chatWindow));
+		setSize(400,200);
+		setVisible(true);
 	}
-	  try {
-          while (!finished) {
-              Socket socket = listener.accept();
-              try {
-                  PrintWriter out =
-                      new PrintWriter(socket.getOutputStream(), true);
-                  out.println(new Date().toString());
-              } 
-              catch (IOException e){
-            	  e.printStackTrace();
-              }
-              
-              
-          }
-      }
-	  catch (IOException e){
-		  e.printStackTrace();
-	  }
-      finally {
-          try {
-			listener.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
+	
+	private void sendMessage(String message) {
+		// TODO Auto-generated method stub
+		try{
+			output.writeObject("Server: " + message);
+			output.flush();
+			showMessage("\n Server: " + message);
+		} catch(IOException e){
+			chatWindow.append("\n Something messed up");
+		}
+	}
+	
+	public void start(){
+		try{
+			server = new ServerSocket(portNumber, maxQueueLength);
+			while (!finished){
+				try{
+					waitForConnection();
+					seUpStreams();
+					whileChatting();
+				} catch (EOFException e){
+					showMessage("\nServer ended the connection");
+				}
+				finally {
+					closeDown();
+				}
+			}
+		} catch(IOException e){}
+	}
+
+	private void closeDown() {
+		// TODO Auto-generated method stub
+		showMessage ("\n Closing down");
+		ableToType(false);
+		try{
+			output.close();
+			input.close();
+			connection.close();
+		} catch (IOException e){
 			e.printStackTrace();
 		}
-      }
-  }
-}
+	}
+
+	private void ableToType(final boolean b) {
+		// TODO Auto-generated method stub
+		SwingUtilities.invokeLater(new Runnable(){
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				textField.setEditable(b);;
+			}
+			
+		});
+	}
+
+	private void showMessage(final String text) {
+		// TODO Auto-generated method stub
+		SwingUtilities.invokeLater(new Runnable(){
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				chatWindow.append(text);
+			}
+			
+		});
+	}
+
+	private void whileChatting() throws IOException{
+		// TODO Auto-generated method stub
+		String message = "You are now connected";
+		sendMessage(message);
+		ableToType(true);
+		do{
+			try{
+				message=(String) input.readObject();
+				showMessage("\n"+message);
+			}
+			catch(ClassNotFoundException e){
+				showMessage("Cannot receive message!");
+			}
+		} while(!message.equals("ME - BYE"));
+	}
+
+	private void seUpStreams() throws IOException{
+		// TODO Auto-generated method stub
+		output = new ObjectOutputStream(connection.getOutputStream());
+		output.flush();
+		input = new ObjectInputStream(connection.getInputStream());
+		showMessage("\n Streams are now setup!");
+	}
+
+	private void waitForConnection() throws IOException{
+		// TODO Auto-generated method stub
+		showMessage("Waiting for someone to connect ... ");
+		connection = server.accept();
+		showMessage("Now connected to "+connection.getInetAddress().getHostName());
+	}
+
+	
+}	
