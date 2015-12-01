6a4f27e213433d7c6d8dc1d86cff3afd70f73a11
Dylan Chen <hjchen2005@gmail.com>
2015-12-01 14:41:22 -0500

    Update Program.java

--- Server/Program.java
+++ Server/Program.java
@@ -1,11 +1,14 @@
 package Server;
 
+import javax.swing.JFrame;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Server server = new Server();
-		server.run();
+		server.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		server.start();
 	}
 
 }
